06ef92ac89f0089b32064aaec464e2ccd127c421
LouiseHopfner <lhoepfner@gmail.com>
2018-04-03 22:45:14 +0200

    added frontend handling with npm and gulp

---
+++ PizzaApp/gulpfile.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+var _ = require('lodash'),
+    gulp = require('gulp');
+
+gulp.task('copy-assets', function () {
+    var assets = {
+        js: [
+            './node_modules/bootstrap/dist/js/bootstrap.js'
+        ],
+        css: ['./node_modules/bootstrap/dist/css/bootstrap.css']
+    };
+    _(assets).forEach(function (assets, type) {
+        gulp.src(assets).pipe(gulp.dest('./wwwroot/' + type));
+    });
+});
